11942c04be8761aa41b7a421727f911eb499881c
Mikhail Bulygin <pasaranax@gmail.com>
2018-04-06 14:24:17 +0300

    add requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice",
-    version="1.0",
-    packages=find_packages()
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "aiopg==0.13.2",
+        "oauth2==1.9.0.post1",
+        "peewee==2.10.2",
+        "peewee-async==0.5.10",
+        "prometheus-client==0.1.1",
+        "pyTelegramBotAPI==3.5.1",
+        "raven==6.5.0",
+        "requests==2.18.4",
+        "tornado==4.4",
+        "ua-parser==0.7.3",
+        "urllib3==1.22",
+        "google-auth==1.0.2",
+        "slackclient==1.1.2",
+        "pytz==2018.3",
+    ]
 )
