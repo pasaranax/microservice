111f6ccdb08664689c077bf04bb5f014b6fb0909
Mikhail Bulygin <pasaranax@gmail.com>
2018-04-09 19:03:04 +0300

    silent metrics handler

--- microservice/handlers/metrics.py
+++ microservice/handlers/metrics.py
@@ -1,5 +1,3 @@
-import logging
-
 from prometheus_client import generate_latest
 from tornado import gen
 
@@ -15,13 +13,4 @@ class MetricsHandler(BasicHandler):
 
     @gen.coroutine
     def on_finish(self):
-        """
-        Выполняется после отправки ответа
-        """
-        for item in self.queue:
-            yield from self.loop.create_task(item)
-        log_record = self.make_log()
-        if str(self.get_status()).startswith("4"):
-            logging.warning(log_record)
-        elif str(self.get_status()).startswith("5"):
-            logging.error(log_record)
+        pass
