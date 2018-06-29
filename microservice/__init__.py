3344b2bfcb5e7ecf809cad2788cd701260e1e5ea
Mikhail Bulygin <pasaranax@gmail.com>
2018-06-29 18:47:44 +0300

    fix import

--- microservice/__init__.py
+++ microservice/__init__.py
@@ -1,5 +1,5 @@
 from microservice.handlers.container import Data
 from microservice.handlers.handler import BasicHandler
-from microservice.managers.manager import DataManager
+from microservice.managers.manager import DataManager, Collection, BasicObject
 from microservice.middleware.session import check
 from microservice.server import Server
