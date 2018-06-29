e35f365228e325fe32ff34a1724b824890f359cd
Mikhail Bulygin <pasaranax@gmail.com>
2018-06-29 19:11:22 +0300

    objects move

--- microservice/__init__.py
+++ microservice/__init__.py
@@ -1,5 +1,6 @@
 from microservice.handlers.container import Data
 from microservice.handlers.handler import BasicHandler
-from microservice.managers.manager import DataManager, Collection, BasicObject
+from microservice.managers.manager import DataManager
 from microservice.middleware.session import check
 from microservice.server import Server
+from managers.objects import BasicObject, Collection
--- microservice/managers/manager.py
+++ microservice/managers/manager.py
@@ -1,44 +1,6 @@
 from datetime import timedelta, datetime
 
 
-from collections import UserDict, UserList
-
-
-class BasicObject(UserDict):
-    def __init__(self, item_dict):
-        super(BasicObject, self).__init__(item_dict)
-        self.data.update(item_dict)
-        self.validate()
-        for key in self.data:
-            setattr(self, key, self.data[key])
-
-    def __setitem__(self, key, value):
-        self.data[key] = value
-        setattr(self, key, value)
-
-    def validate(self):
-        """implement it"""
-        pass
-
-
-class Collection(UserList):
-    def __init__(self, items_list, object_class=None):
-        super(Collection, self).__init__(items_list)
-        self.object_class = object_class
-        self.items = items_list
-        if object_class:
-            self.set_class(object_class)
-        else:
-            self.data = []
-            self.valid = False
-
-    def set_class(self, object_class):
-        self.object_class = object_class
-        for item in self.items:
-            self.data.append(object_class(item))
-        self.valid = True
-
-
 class DataManager:
     """
     Менеджер данных выполняет работу с данными в формате CRUD+L(list)
