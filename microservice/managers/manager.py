d53779624e9538b808fbd5b14cb85fe656bdbcb5
Mikhail Bulygin <pasaranax@gmail.com>
2018-06-29 18:39:53 +0300

    fix Collection

--- microservice/managers/manager.py
+++ microservice/managers/manager.py
@@ -35,7 +35,7 @@ class Collection(UserList):
     def set_class(self, object_class):
         self.object_class = object_class
         for item in self.items:
-            self.data.append(item.validate)
+            self.data.append(object_class(item))
         self.valid = True
 
 
