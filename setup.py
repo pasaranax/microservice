97835ddd4f43968ef6dcb192b5c4f865c30c00b4
Mikhail Bulygin <pasaranax@gmail.com>
2018-07-12 21:43:50 +0300

    0.2.24

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice",
-    version="0.2.23",
+    version="0.2.24",
     description="Powerful REST API microservice on Tornado",
     url="https://git.phobos.work/mbulygin/microservice",
     author="Mikhail Bulygin",
