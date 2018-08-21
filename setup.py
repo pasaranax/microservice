da2fe47e36c107ec35108f5a56c07ef642fea1b2
Mikhail Bulygin <pasaranax@gmail.com>
2018-08-21 16:56:28 +0300

    0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice",
-    version="0.2.40",
+    version="0.3.0",
     description="Powerful REST API microservice on Tornado",
     url="https://github.com/pasaranax/microservice",
     author="Mikhail Bulygin",
