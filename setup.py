fd933178bcb876cbc819f55ac82ca4dbbf0db0ec
Mikhail Bulygin <pasaranax@gmail.com>
2018-09-27 14:45:15 +0300

    0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice",
-    version="0.3.3",
+    version="0.3.4",
     description="Powerful REST API microservice on Tornado",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
