4a21ec1edd02aa278438ca2787336f9f48085f89
Mikhail Bulygin <pasaranax@gmail.com>
2018-10-17 19:31:08 +0300

    0.3.10 requirements versions ranges

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice",
-    version="0.3.9",
+    version="0.3.10",
     description="Powerful REST API microservice on Tornado",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -20,20 +20,20 @@ setup(
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
     ],
     install_requires=[
-        "aiopg==0.13.2",
-        "oauth2==1.9.0.post1",
-        "peewee==2.10.2",
-        "peewee-async==0.5.12",
-        "prometheus-client==0.3.0",
-        "pyTelegramBotAPI==3.6.5",
-        "raven==6.9.0",
-        "requests==2.18.4",
-        "tornado==5.1",
-        "ua-parser==0.8.0",
-        "urllib3==1.22",
-        "google-auth==1.5.0",
-        "pytz==2018.5",
-        "aioredis==1.1.0",
-        "asgiref==2.3.2"
+        "aiopg>=0.13.2,<0.14",
+        "oauth2>=1.9.0.post1,<1.10",
+        "peewee>=2.10.2,<2.11",
+        "peewee-async>=0.5.12",
+        "prometheus-client>=0.3.0,<0.4",
+        "pyTelegramBotAPI>=3.6.5,<3.7",
+        "raven>=6.9.0,<6.10",
+        "requests>=2.19,<2.20",
+        "tornado>=5.1,<6",
+        "ua-parser>=0.8.0,<0.9",
+        "urllib3>=1.22,<1.23",
+        "google-auth>=1.5.0,<1.6",
+        "pytz>=2018.5",
+        "aioredis>=1.1.0,<1.2",
+        "asgiref>=2.3.2,<2.4"
     ]
 )
