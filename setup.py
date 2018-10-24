7ec75f3b70bbf383401fca55b91775259d525f8e
pasaranax <pasaranax@gmail.com>
2018-10-24 13:12:59 +0300

    update dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice",
-    version="0.3.10",
+    version="0.3.11",
     description="Powerful REST API microservice on Tornado",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -20,17 +20,17 @@ setup(
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
     ],
     install_requires=[
-        "aiopg>=0.13.2,<0.14",
+        "aiopg>=0.13.2,<1",
         "oauth2>=1.9.0.post1,<1.10",
         "peewee>=2.10.2,<3",
         "peewee-async>=0.5.12,<0.6",
-        "prometheus-client>=0.3.0,<0.4",
+        "prometheus-client>=0.3.0,<1",
         "pyTelegramBotAPI>=3.6.5,<3.7",
         "raven>=6.9.0,<6.10",
-        "requests>=2.19,<2.20",
+        "requests>=2.19,<3",
         "tornado>=5.1,<6",
-        "ua-parser>=0.8.0,<0.9",
-        "urllib3>=1.22,<1.23",
+        "ua-parser>=0.8.0,<1",
+        "urllib3>=1.22,<2",
         "google-auth>=1.5.0,<1.6",
         "pytz>=2018.5",
         "aioredis>=1.1.0,<1.2",
