1c9d86ae13aca2c81e47d7b8e43ca0e5ef219b96
belegnar <admin@zilantkon.ru>
2019-01-10 03:01:04 +0300

    * doesn't work

--- microservice/server.py
+++ microservice/server.py
@@ -12,11 +12,10 @@ from microservice.routes import Router
 
 
 class Server:
-    def __init__(self, handlers, asyncio_debug=False):
+    def __init__(self, handlers):
         logging.info("Server starting...")
         AsyncIOMainLoop().install()
         self.loop = asyncio.get_event_loop()
-        self.loop.set_debug(asyncio_debug)
 
         self.router = Router(handlers)
         self.app = Application(self.router.routes, **cfg.app.tornado_settings)
