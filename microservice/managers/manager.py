b52f71e1bdc59382f029d9bc4ddf8c381c2bffb9
belegnar <admin@zilantkon.ru>
2019-04-16 14:17:36 +0300

    do not compute len in extract_results

--- microservice/managers/manager.py
+++ microservice/managers/manager.py
@@ -64,7 +64,7 @@ class DataManager:
         :param results: ответ эластика или пиви
         :return: list
         """
-        if len(list(islice(results, 0))) > 0:
+        if len(list(islice(results, 0, 1))) > 0:
             lst = Collection([x.dict(*args, **kwargs) for x in results], BasicObject)
         else:
             lst = Collection([], BasicObject)
