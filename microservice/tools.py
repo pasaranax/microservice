1f33e6c8cd076fee5030416f9405705b95bba909
pasaranax <pasaranax@gmail.com>
2019-04-27 17:03:50 +0300

    0.3.31 auto create migrations table on first migration (empty database)

--- microservice/tools.py
+++ microservice/tools.py
@@ -4,8 +4,9 @@ import pkgutil
 import sys
 
 import migrations
-from microservice.models import connection
 from models import Migrations
+
+from microservice.models import connection
 
 
 def migrate():
@@ -13,6 +14,22 @@ def migrate():
     Функция для проверки и применения миграций.
     """
     logging.info("Trying migrations")
+    connection.execute_sql("""
+        create table if not exists migrations
+        (
+          id   serial       not null
+            constraint migrations_pk
+              primary key,
+          name varchar(200) not null,
+          time timestamp
+        );
+        
+        alter table migrations
+          owner to ege;
+        
+        create unique index if not exists migrations_id_uindex
+          on migrations (id);
+    """)
     applied_migrations = [x.name for x in Migrations.select().order_by(Migrations.name).asc()]
     last_migration = applied_migrations[-1] if len(applied_migrations) > 0 else "Database is empty"
     for loader, name, _ in pkgutil.walk_packages(migrations.__path__):
